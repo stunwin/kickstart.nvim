4358726539194acc27ef630037b76bba4e7cd903
stunwin <steve.unwin@gmail.com>
2024-09-11 19:25:24 -0700

    theme stuf

--- init.lua
+++ init.lua
@@ -135,6 +135,9 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- turning off the default bind so that mini surround works
 vim.keymap.set('n', 's', '', { desc = '[S]urround' })
 
+-- NOTE: this lil chunk here is to disable autoformatting when working on files with
+-- weirdly-formatted arrays like keymaps
+--
 -- Initialize flag to track the state
 local format_enabled = true
 
@@ -792,6 +795,15 @@ require('lazy').setup(
         }
       end,
     },
+    {
+      'navarasu/onedark.nvim',
+      priority = 1000,
+      opts = {
+        style = 'deep',
+      },
+
+      -- You can configure highlights by doing something like:
+    },
 
     { -- You can easily change to a different colorscheme.
       -- Change the name of the colorscheme plugin below, and then
@@ -799,18 +811,15 @@ require('lazy').setup(
       --
       -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
       'folke/tokyonight.nvim',
-      priority = 1000, -- Make sure to load this before all the other start plugins.
+      priority = 999, -- Make sure to load this before all the other start plugins.
       init = function()
+        vim.cmd.colorscheme 'onedark'
         -- Load the colorscheme here.
+        vim.cmd.hi 'Comment gui=none'
         -- Like many other themes, this one has different styles, and you could load
         -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-        vim.cmd.colorscheme 'tokyonight-night'
-
-        -- You can configure highlights by doing something like:
-        vim.cmd.hi 'Comment gui=none'
       end,
     },
-
     -- Highlight todo, notes, etc in comments
     { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
